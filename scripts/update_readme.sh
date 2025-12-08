fa6a2131dea4ca743f49f1bd464fb52cc46f2f47
AmitLY21 <Amit.Levy@appsflyer.com>
2025-12-08 11:38:51 +0200

    fix: BSD sed compatibility in update_readme.sh
    - Split single sed command with alternation into two separate commands
    - Fixes 'parentheses not balanced' error on macOS
    - Tested and verified working

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -6,8 +6,13 @@ VERSION="$1"
 README="README.md"
 
 # Update Carthage installation examples to use the new version
+# Use two separate sed commands to avoid BSD sed issues with alternation in capture groups
 sed -i.bak -E \
-  "s|(AppsFlyerRPC-(static|dynamic)\.json\" == )[0-9]+\.[0-9]+\.[0-9]+|\1${VERSION}|g" \
+  "s|AppsFlyerRPC-static\.json\" == [0-9]+\.[0-9]+\.[0-9]+|AppsFlyerRPC-static.json\" == ${VERSION}|g" \
+  "$README"
+
+sed -i.bak -E \
+  "s|AppsFlyerRPC-dynamic\.json\" == [0-9]+\.[0-9]+\.[0-9]+|AppsFlyerRPC-dynamic.json\" == ${VERSION}|g" \
   "$README"
 
 # Cleanup backup file
